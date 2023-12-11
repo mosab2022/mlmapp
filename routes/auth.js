3fadf14c47d9be9b775dbc49fddd5e3e638b4e6f
mosab2022 <111149608+mosab2022@users.noreply.github.com>
2023-12-11 20:43:43 +0200

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcrypt");
 const authRouter = express.Router();
-
+const jwt = require("jsonwebtoken");
 // SIGN UP
 authRouter.post("/api/signup", async (req, res) => {
   try {
@@ -28,6 +28,28 @@ authRouter.post("/api/signup", async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 });
+// Sign In Route
+// Exercise
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email does not exist!" });
+    }
+
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect password." });
+    }
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ token, ...user._doc });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});   
 
 module.exports = authRouter;
